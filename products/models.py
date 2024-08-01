db83eaa8d7ffbe4d48dc216e548e90d89b13c6a0
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-01 23:38:22 +0330

    feat(products): create brand and mobile models

--- products/models.py
+++ products/models.py
@@ -1,10 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
+from django_lifecycle import BEFORE_SAVE, LifecycleModelMixin, hook
+
+from common.models import COUNTRY, BaseModel
 
 
-class Country(models.TextChoices):
-    
+class Brand(LifecycleModelMixin, models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    country = models.CharField(max_length=2, choices=COUNTRY)
 
-class Brand(models.Model): ...
+    @hook(BEFORE_SAVE)
+    def set_slug(self):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class Mobile(models.Model): ...
+class Mobile(BaseModel):
+    model = models.CharField(max_length=255, unique=True)
+    price = models.PositiveIntegerField()
+    color = models.CharField(max_length=255)
+    size_screen = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
+    manufacturer = models.CharField(max_length=2, choices=COUNTRY)
+    brand = models.ForeignKey(
+        to=Brand,
+        on_delete=models.CASCADE,
+        related_name="mobiles",
+    )
+
+    def __str__(self) -> str:
+        self.model
