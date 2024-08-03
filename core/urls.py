8d9f6a73d4044f70682bc70a9e01b7b6c74d2644
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-03 19:21:52 +0330

    feat(products): add create brand form

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include("common.urls", namespace="common")),
+    path("admin/", admin.site.urls),
+    path("products/", include("products.urls")),
 ]
 
 if "debug_toolbar" in settings.INSTALLED_APPS:
