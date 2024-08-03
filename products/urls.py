31023d2c66d8b877dd413ed8bf5df4cd11b020a7
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-03 23:32:30 +0330

    feat: add list of korean brands

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 
-from .views import AddBrandView, AddMobileView
+from .views import AddBrandView, AddMobileView, KoreanBrandListView
 
 app_name = "products"
 
 brand_urls = [
     path("add", AddBrandView.as_view(), name="add-brand"),
+    path("korean", KoreanBrandListView.as_view(), name="korean-brands"),
 ]
 product_urls = [
     path("add", AddMobileView.as_view(), name="add-mobile"),
--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
 
 from common.mixins import AdminRequiredMixin
+from common.models import COUNTRY
 
 from .forms import BrandForm, MobileForm
+from .models import Brand, Mobile
 
 
 class AddBrandView(AdminRequiredMixin, View):
@@ -89,3 +92,12 @@ class AddMobileView(AdminRequiredMixin, View):
             template_name=self.template_name,
             context=context,
         )
+
+
+class KoreanBrandListView(ListView):
+    template_name = "products/brand_list.html"
+    context_object_name = "objects"
+    model = Brand
+
+    def get_queryset(self):
+        return self.model.objects.filter(country=COUNTRY.KR)
