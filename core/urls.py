9e19a34801b0be525e5ea9cd03d2aa7029806633
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-03 22:39:59 +0330

    feat: add create user form in admin panel

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("common.urls", namespace="common")),
     path("admin/", admin.site.urls),
-    path("products/", include("products.urls")),
+    path("products/", include("products.urls", namespace="products")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
 ]
 
 if "debug_toolbar" in settings.INSTALLED_APPS:
