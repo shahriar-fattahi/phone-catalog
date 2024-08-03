45d4b023a38660b56693afab2cd0342370cf2ef3
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-03 19:58:38 +0330

    fix(common): change AdminRequireMixin

--- common/mixins.py
+++ common/mixins.py
@@ -5,7 +5,7 @@ class AdminRequiredMixin(AccessMixin):
     """Verify that the current user is authenticated."""
 
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            if not request.user.is_admin:
-                return self.handle_no_permission()
-        return super().dispatch(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            if request.user.is_admin:
+                return super().dispatch(request, *args, **kwargs)
+        return self.handle_no_permission()
